52e002e2ae4d9025481ee3457fbd3682da8a5869
Victor <victor.jgfsilva@gmail.com>
2021-04-21 16:25:04 -0300

    Added sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,22 @@
 import express from 'express';
+import './database';
 
 const app = express();
 
-app.listen(3000, () => {
-	console.log('Hello World!');
+const PORT = 3333;
+
+app.get('/', (request, response) => {
+	return response.json({
+		message: 'Olá NLW05!',
+	});
+});
+
+app.post('/users', (request, response) => {
+	return response.json({
+		message: 'Usuário salvo com sucesso!',
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
 });
