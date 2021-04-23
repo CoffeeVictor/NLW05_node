9d8ffa68e767cc3668fa04df1440b4d060a493d4
Victor <victor.jgfsilva@gmail.com>
2021-04-23 03:21:12 -0300

    Created routes

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,14 @@
 import express from 'express';
 import './database';
+import { routes } from './routes';
 
 const app = express();
 
 const PORT = 3333;
 
-app.get('/', (request, response) => {
-	return response.json({
-		message: 'Olá NLW05!',
-	});
-});
+app.use(express.json());
 
-app.post('/users', (request, response) => {
-	return response.json({
-		message: 'Usuário salvo com sucesso!',
-	});
-});
+app.use(routes);
 
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
