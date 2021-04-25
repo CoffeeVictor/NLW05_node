4d14ee3f19905c94a8f595a2c7ce018695e62a17
Victor <victor.jgfsilva@gmail.com>
2021-04-25 02:50:54 -0300

    Added sockets

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import express from 'express';
 import './database';
 import { routes } from './routes';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 
 const app = express();
+
+const http = createServer(app);
+const io = new Server(http);
+
+io.on('connection', (socket: Socket) => {
+	console.log(`Connecter to socket. Id: ${socket.id}`);
+});
 
 const PORT = 3333;
 
@@ -10,6 +19,6 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(PORT, () => {
+http.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
 });
